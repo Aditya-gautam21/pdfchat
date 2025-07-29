6b106cf2b42781c7db2eabdfddddaa16e3ee786c
Aditya-gautam21 <153100646+Aditya-gautam21@users.noreply.github.com>
2025-07-29 14:20:28 +0530

    Updated

--- prompt.py
+++ prompt.py
@@ -5,52 +5,52 @@ class Prompt:
     def provide_prompt():
         # Question generator prompt (ONLY 'question')
         question_generator_prompt = PromptTemplate(
-        input_variables=["question", "chat_history"],
-        template="""
-        Given the chat history and a follow-up question, rephrase the follow-up into a standalone question.
+            input_variables=["question", "chat_history"],
+            template="""
+            Given the chat history and a follow-up question, rephrase the follow-up into a standalone question.Only answer the question in reference to the provided document and understand that any question that is asked is in reference to the pdf.
 
-        Chat history:
-        {chat_history}
+            Chat history:
+            {chat_history}
 
-        Follow-up question:
-        {question}
+            Follow-up question:
+            {question}
 
-        Standalone question:
-        """
-        )
+            Standalone question:
+            """
+            )
 
 
         # Initial answering prompt (NEEDS 'context' + 'question')
         question_prompt = PromptTemplate(
             input_variables=["context", "question"],
             template="""
-Use the following context to answer the question clearly and directly but do not make it too short.
+            Use the following context to answer the question clearly and directly but do not make it too short or too long.Only answer the question in reference to the provided document and understand that any question that is asked is in reference to the pdf.
 
-Context:
-{context}
+            Context:
+            {context}
 
-Question:
-{question}
+            Question:
+            {question}
 
-Answer:"""
-        )
+            Answer:"""
+            )
 
         # Refine prompt (NEEDS 'context' + 'question' + 'existing_answer')
         refine_prompt = PromptTemplate(
             input_variables=["context", "question", "existing_answer"],
             template="""
-Improve the existing answer using the additional context below if necessary and only return the answer.
+            Improve the existing answer using the additional context below if necessary and only return the answer.Only answer the question in reference to the provided document and understand that any question that is asked is in reference to the pdf.
 
-Context:
-{context}
+            Context:
+            {context}
 
-Question:
-{question}
+            Question:
+            {question}
 
-Existing Answer:
-{existing_answer}
+            Existing Answer:
+            {existing_answer}
 
-Improved Answer:"""
-        )
+            Improved Answer:"""
+            )
 
         return question_generator_prompt, question_prompt, refine_prompt
